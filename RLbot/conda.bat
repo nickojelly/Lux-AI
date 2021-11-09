46638c9ae143d8eb534374d2b17639f21df3a899
nickojelly <72242022+nickojelly@users.noreply.github.com>
2021-11-09 13:31:42 +1100

    Initial Commit to bring in offline files

---
+++ RLbot/conda.bat
@@ -0,0 +1,4 @@
+setlocal
+call C:\ProgramData\Anaconda3\condabin\conda_hook.bat
+call conda activate py37
+C:\Users\Nick\.conda\envs\py37\python.exe   "C:\Users\Nick\Documents\GitHub\Lux-AI\RLbot\Testing Bot\train.py"
--- luxrun.py
+++ luxrun.py
@@ -4,11 +4,4 @@ import time
 seed = 471756362
 seed2 =  297064116
 i = 0
-while i < 20:
-    os.system(f'lux-ai-2021 "submission bots\\bot v0.6\main.py" "attempt 3\main.py" --out=file.json{i}') 
-    print(f"Finsished game {i}")
-    i+=1
-    break
-
-
-#os.startfile("attempt 2/basicLog.log")
+os.system(f'lux-ai-2021 --rankSystem="trueskill" --tournament --maxConcurrentMatches=2 --storeReplay=false --storeLogs=false "simple.tar\main.py" "bot v0.3\main.py" "bot v0.7\main.py" "submission bots\\bot v0.6\main.py" "attempt 3\main.py" ') 
